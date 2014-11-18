e67a22726dec547440d7b1c40d9589e8f559ebaa
Marcus Rocha <marcusorocha@gmail.com>
2014-11-18 19:33:11 -0200

    Personalizações da interface de login e no menu de opções. Alterado o logo para o Logo da Furb com fundo transparente.

--- AvaMobile/gen/br/furb/avamobile/R.java
+++ AvaMobile/gen/br/furb/avamobile/R.java
@@ -22,18 +22,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_shape=0x7f020000;
+        public static final int edit_shape=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int atualizar=0x7f080007;
-        public static final int btnConsultarFinanceiro=0x7f080004;
-        public static final int editNome=0x7f080001;
-        public static final int editSenha=0x7f080003;
-        public static final int lbNome=0x7f080000;
-        public static final int lbSenha=0x7f080002;
-        public static final int lbUsername=0x7f080005;
-        public static final int txtUsername=0x7f080006;
+        public static final int bottomSeparator=0x7f080002;
+        public static final int btnConsultarFinanceiro=0x7f080007;
+        public static final int editNome=0x7f080004;
+        public static final int editSenha=0x7f080006;
+        public static final int imgFurb=0x7f080001;
+        public static final int lbNome=0x7f080003;
+        public static final int lbSenha=0x7f080005;
+        public static final int lbUsername=0x7f080008;
+        public static final int topSeparator=0x7f080000;
+        public static final int txtUsername=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,17 +48,16 @@ public final class R {
         public static final int menu=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int acessar=0x7f050009;
         public static final int app_name=0x7f050000;
-        public static final int atualizar=0x7f050003;
-        public static final int consultar_financeiro=0x7f05000a;
-        public static final int hello_world=0x7f050001;
-        public static final int lb_username=0x7f050009;
-        public static final int login=0x7f050007;
-        public static final int nome=0x7f050004;
-        public static final int senha=0x7f050005;
-        public static final int title_activity_menu=0x7f050008;
-        public static final int token=0x7f050006;
+        public static final int consultar_financeiro=0x7f050007;
+        public static final int lb_username=0x7f050006;
+        public static final int login=0x7f050004;
+        public static final int logo_furb=0x7f050008;
+        public static final int nome=0x7f050001;
+        public static final int senha=0x7f050002;
+        public static final int title_activity_menu=0x7f050005;
+        public static final int token=0x7f050003;
     }
     public static final class style {
         /** 
